a5bc23641421b3fd897c0c90854a096232b1c2de
Alwurts <jota780@gmail.com>
2025-02-18 22:35:29 +0100

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 function App() {
 	return (
-		<div className="bg-red-500">
-			<h1>Hello World</h1>
+		<div className="flex justify-center items-center h-screen">
+			<Card>
+				<CardHeader>
+					<CardTitle>Hello World</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Button>Click me</Button>
+				</CardContent>
+			</Card>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), cloudflare(),tailwindcss()],
+	plugins: [react(), cloudflare(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
